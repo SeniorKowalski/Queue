786904207f9bec80c8c181b1c2db6b45aab293d1
Egor Laptev <laptencio@yandex.ru>
2022-11-08 18:51:09 +0400

    Program works

--- src/com/kowalski/Main.java
+++ src/com/kowalski/Main.java
@@ -1,8 +1,33 @@
 package com.kowalski;
 
+import java.util.*;
+
 public class Main {
 
+    public static List<Person> personList = new ArrayList<>();
+    public static String[] names = {"John", "Bruce", "Rick", "Eric", "Kyle", "Jack", "Sam"};
+    public static String[] surnames = {"Smith", "Wayne", "Stone", "Cartman","Black", "Pinkman", "White"};
+
     public static void main(String[] args) {
-	// write your code here
+        generatePersons(personList);
+        Deque<Person> carousel = new ArrayDeque<>();
+        carousel.addAll(personList);
+        while (!carousel.isEmpty()){
+            if (carousel.getFirst().getTicketQuantity()>0){
+                System.out.println(carousel.getFirst().getName() + " " + carousel.getFirst().getSurname() + " " + carousel.getFirst().getTicketQuantity());
+                carousel.getFirst().setTicketQuantity(carousel.getFirst().getTicketQuantity() - 1);
+                carousel.addLast(carousel.getFirst());
+                carousel.pollFirst();
+            } else {
+                carousel.pollFirst();
+            }
+        }
+    }
+
+    public static List<Person> generatePersons(List<Person> personList){
+        for (int i = 0; i < 7; i++) {
+            personList.add(new Person(names[i],surnames[i]));
+        }
+        return personList;
     }
 }
--- src/com/kowalski/Person.java
+++ src/com/kowalski/Person.java
@@ -1,4 +1,31 @@
 package com.kowalski;
 
+import java.util.Random;
+
 public class Person {
+    private Random random = new Random();
+    private String name, surname;
+    private int ticketQuantity;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+        this.ticketQuantity = random.nextInt(1,5);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getTicketQuantity() {
+        return ticketQuantity;
+    }
+
+    public void setTicketQuantity(int ticketQuantity) {
+        this.ticketQuantity = ticketQuantity;
+    }
 }
